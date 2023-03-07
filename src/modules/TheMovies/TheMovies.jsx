7ba2e104f8f093161d77bbe10d7965d4f724caab
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-03-07 02:02:15 +0200

    fix gh

--- src/modules/TheMovies/TheMovies.jsx
+++ src/modules/TheMovies/TheMovies.jsx
@@ -1,4 +1,4 @@
-// import { useState,useEffect,useCallback } from "react"
+// import { useState,useEffect,useCallback } from "react"goit-react-hw-05-movies
 import Movies from "./Movies/Movies";
 
 const TheMovies = () => {
