d47ace068b3c18959dd60f449d9eb9e57f88db61
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-03-15 08:12:29 +0200

    Update MovieDetails.jsx

--- src/modules/TheMovies/Movies/MovieDetails/MovieDetails.jsx
+++ src/modules/TheMovies/Movies/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getFilmInfo, getPoster } from 'shared/services/TheMoviesAPI';
+import { getFilmInfo } from 'shared/services/TheMoviesAPI';
 
 
 const MovieDetails = () => {
